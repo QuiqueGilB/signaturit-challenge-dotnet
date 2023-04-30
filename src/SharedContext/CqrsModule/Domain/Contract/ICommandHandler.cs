fe4a51308a805d9f5b78796ae0e65ee8caae7508
Enrique Gil <user@enriquegil.me>
2023-04-30 21:01:20 +0200

    fix: Compatibilities

--- src/SharedContext/CqrsModule/Domain/Contract/ICommandHandler.cs
+++ src/SharedContext/CqrsModule/Domain/Contract/ICommandHandler.cs
@@ -2,11 +2,17 @@ using app.SharedContext.CqrsModule.Domain.Model;
 
 namespace app.SharedContext.CqrsModule.Domain.Contract;
 
-public interface ICommandHandler<in T> where T: Command
+public interface ICommandHandler<in T> : ICommandHandler where T : Command
 {
+    Task ICommandHandler.Handle(Command command)
+    {
+        return Handle((T)command);
+    }
+
     public Task Handle(T command);
 }
 
-public interface ICommandHandler: ICommandHandler<Command>
+public interface ICommandHandler
 {
+    public Task Handle(Command command);
 }
