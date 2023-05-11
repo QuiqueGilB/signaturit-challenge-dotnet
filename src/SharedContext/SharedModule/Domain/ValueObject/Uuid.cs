29c48a35af648a2fb4a3a9d6ecf4523fc93b569c
Enrique Gil <user@enriquegil.me>
2023-05-11 22:33:50 +0200

    fix: regex validation

--- src/SharedContext/SharedModule/Domain/ValueObject/Uuid.cs
+++ src/SharedContext/SharedModule/Domain/ValueObject/Uuid.cs
@@ -6,11 +6,15 @@ namespace app.SharedContext.SharedModule.Domain.ValueObject;
 
 public record Uuid(string Value) : ValueObject<string>(Value)
 {
-    private const string UuidRegex = "/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i";
+    private static readonly Regex _uuidRegex;
+    static Uuid()
+    {
+        _uuidRegex = new Regex(@"^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$", RegexOptions.IgnoreCase);
+    }
 
     public override void Validate()
     {
-        if (!Regex.IsMatch(UuidRegex, Value))
+        if (!_uuidRegex.IsMatch(Value))
         {
             throw InvalidUuidException.ByValue(Value);
         }
