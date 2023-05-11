8df683f61ae2b437faa74931ffcdb29c04269b6c
Enrique Gil <user@enriquegil.me>
2023-05-11 21:33:25 +0200

    refactor: lint

--- tests/Unit/ContractContext/ContractModule/Domain/Service/ResolveSignatureScoresServiceTest.cs
+++ tests/Unit/ContractContext/ContractModule/Domain/Service/ResolveSignatureScoresServiceTest.cs
@@ -14,7 +14,6 @@ public class ResolveSignatureScoresServiceTest
         _resolveSignatureScoresService = new ResolveSignatureScoresService();
     }
 
-
     [Theory]
     [MemberData(nameof(StagesTestAccumulateScore))]
     public void TestAccumulateScore(Score score, IEnumerable<Signature> signatures)
@@ -24,15 +23,15 @@ public class ResolveSignatureScoresServiceTest
 
     public static IEnumerable<object[]> StagesTestAccumulateScore()
     {
-        yield return new object[] { new Score(5), new[] { Signature.King } }; //one king'
-        yield return new object[] { new Score(2), new[] { Signature.Notary } }; //one notary'
-        yield return new object[] { new Score(1), new[] { Signature.Validator } }; //one validator'
-        yield return new object[] { new Score(0), new[] { Signature.Unknown } }; //one unknown'
-        yield return new object[] { new Score(7), new[] { Signature.King, Signature.Notary } }; //king-notary'
+        yield return new object[] { new Score(5), new[] { Signature.King } }; //one king
+        yield return new object[] { new Score(2), new[] { Signature.Notary } }; //one notary
+        yield return new object[] { new Score(1), new[] { Signature.Validator } }; //one validator
+        yield return new object[] { new Score(0), new[] { Signature.Unknown } }; //one unknown
+        yield return new object[] { new Score(7), new[] { Signature.King, Signature.Notary } }; //king-notary
         yield return new object[]
             { new Score(7), new[] { Signature.King, Signature.Notary, Signature.Validator } }; //king-notary-validator
         yield return new object[]
             { new Score(5), new[] { Signature.King, Signature.Unknown, Signature.Validator } }; //king-unknown-validator
-        yield return new object[] { new Score(3), new[] { Signature.Validator, Signature.Notary } }; //validator-notary'
+        yield return new object[] { new Score(3), new[] { Signature.Validator, Signature.Notary } }; //validator-notary
     }
 }
